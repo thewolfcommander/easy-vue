b4bedf8b48e715d56713ba13b53774540dbdb481
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-07-04 22:02:17 +0530

    Completed Grocery Designs

--- src/views/products/food/Categories.vue
+++ src/views/products/food/Categories.vue
@@ -9,7 +9,7 @@
                     <h2 class="text-h4 grey--text">Food Categories</h2>
                 </v-col>
             </v-row>
-            <v-row justify="center">
+            <v-row justify="center" class="d-none d-md-flex">
                 <v-col cols="12">
                     <v-card
                         flat
@@ -23,16 +23,32 @@
                     </v-card>
                 </v-col>
             </v-row>
+            <v-row justify="center" class="d-flex d-md-none">
+                <v-col cols="12">
+                    <v-card
+                        flat
+                        class="px-md-5 px-lg-5"
+                    >
+                        <v-row wrap>
+                            <v-col cols="6" md="3" lg="3" v-for="(item, index) in 8" :key="index">
+                                <MobileCategoryCard />
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
     </v-container>
 </template>
 
 <script>
 import CategoryCard from '@/components/Common/CategoryCard'
+import MobileCategoryCard from '@/components/Common/Mobile/SmallFoodCategoryCard'
 
 export default {
     components: {
-        CategoryCard
+        CategoryCard,
+        MobileCategoryCard
     }
 }
 </script>
--- src/views/products/food/Restaurants.vue
+++ src/views/products/food/Restaurants.vue
@@ -9,7 +9,7 @@
                     <h2 class="text-h4 grey--text">Best Restaurants out there</h2>
                 </v-col>
             </v-row>
-            <v-row justify="center">
+            <v-row justify="center" class="d-none d-md-flex">
                 <v-col cols="12">
                     <v-card
                         flat
@@ -23,16 +23,32 @@
                     </v-card>
                 </v-col>
             </v-row>
+            <v-row justify="center" class="d-flex d-md-none">
+                <v-col cols="12">
+                    <v-card
+                        flat
+                        class="px-md-5 px-lg-5"
+                    >
+                        <v-row wrap>
+                            <v-col cols="6" md="3" lg="3" v-for="(item, index) in 8" :key="index">
+                                <MobileRestaurantCard />
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
     </v-container>
 </template>
 
 <script>
-import RestaurantCard from '@/components/Common/Mobile/SmallRestaurantCard'
+import MobileRestaurantCard from '@/components/Common/Mobile/SmallRestaurantCard'
+import RestaurantCard from '@/components/Common/RestaurantCard'
 
 export default {
     components: {
-        RestaurantCard
+        RestaurantCard,
+        MobileRestaurantCard
     }
 }
 </script>
