ca2134eb99d4eab243c2f6cf68f94cae2880701d
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-07-04 17:46:51 +0530

    Completed Grocery Designs

---
+++ src/components/Menu/Mobile/FilterDrawer.vue
@@ -0,0 +1,219 @@
+<template>
+    <v-dialog
+        v-model="dialog"
+        persistent
+    >
+        <v-card>
+            <v-toolbar
+                flat
+                color="primary"
+                dark
+            >
+                <v-toolbar-title>Filter Food Menu</v-toolbar-title>
+            </v-toolbar>
+            <v-tabs vertical>
+                <v-tab>
+                    <v-icon left>mdi-sort</v-icon>
+                    Sort By
+                </v-tab>
+                <v-tab>
+                    <v-icon left>mdi-google-maps</v-icon>
+                    Restaurants
+                </v-tab>
+                <v-tab>
+                    <v-icon left>mdi-food</v-icon>
+                    Categories
+                </v-tab>
+                <v-tab>
+                    <v-icon left>mdi-star</v-icon>
+                    Rating
+                </v-tab>
+                <v-tab>
+                    <v-icon left>mdi-cash-usd</v-icon>
+                    Price
+                </v-tab>
+
+                <v-tab-item>
+                    <v-card flat>
+                        <v-card-text>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >By Name</v-btn>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >By Price</v-btn>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >By Restaurant</v-btn>
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item>
+                    <v-card flat>
+                        <v-card-text>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >Vatika Srinagar</v-btn>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >Zayaka Srinagar</v-btn>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >Retro Rolls</v-btn>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >The Hill Cafe</v-btn>
+                            <v-col class="text-center">
+                                <v-btn
+                                    text
+                                    color="indigo"
+                                    class="text-capitalize"
+                                >View More</v-btn>
+                            </v-col>
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item>
+                    <v-card flat>
+                        <v-card-text>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >Non-Veg</v-btn>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >Pure Veg</v-btn>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >Snacks</v-btn>
+                            <v-btn
+                                text
+                                rounded
+                                color="primary"
+                                class="text-capitalize"
+                            >Hot Beverages</v-btn>
+                            <v-col class="text-center">
+                                <v-btn
+                                    text
+                                    color="indigo"
+                                    class="text-capitalize"
+                                >View More</v-btn>
+                            </v-col>
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item>
+                    <v-card flat>
+                        <v-card-text>
+                            <v-btn text>
+                                <v-rating
+                                    value="5"
+                                    color="secondary"
+                                    x-small
+                                    readonly
+                                ></v-rating>
+                            </v-btn>
+                            <v-btn text>
+                                <v-rating
+                                    value="4"
+                                    color="secondary"
+                                    x-small
+                                    readonly
+                                ></v-rating>
+                            </v-btn>
+                            <v-btn text>
+                                <v-rating
+                                    value="3"
+                                    color="secondary"
+                                    x-small
+                                    readonly
+                                ></v-rating>
+                            </v-btn>
+                            <v-btn text>
+                                <v-rating
+                                    value="2"
+                                    color="secondary"
+                                    x-small
+                                    readonly
+                                ></v-rating>
+                            </v-btn>
+                            <v-btn text>
+                                <v-rating
+                                    value="1"
+                                    color="secondary"
+                                    x-small
+                                    readonly
+                                ></v-rating>
+                            </v-btn>
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item>
+                    <v-card flat>
+                        <v-card-text>
+                            <PriceRangeSlider />
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+            </v-tabs>
+            <v-card-actions>
+                <v-row justify="end">
+                    <v-btn
+                        @click="dialog=false"
+                        color="primary"
+                        class="mr-5 mb-5"
+                        text
+                        rounded
+                    >Close</v-btn>
+                     <v-btn
+                        @click="dialog=false"
+                        color="secondary"
+                        class="mr-5 mb-5"
+                        text
+                        rounded
+                    >Apply</v-btn>
+                </v-row>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+
+import PriceRangeSlider from '@/components/Common/PriceRangeSlider'
+export default {
+    props: ["dialog"],
+    components: {
+        PriceRangeSlider,
+    }
+};
+</script>
--- src/views/products/food/Menu.vue
+++ src/views/products/food/Menu.vue
@@ -1,24 +1,76 @@
 <template>
     <v-container fluid>
-        <v-row wrap justify="space-between">
-            <v-col cols="12" md="3" lg="3">
-                <FilterSection />
+        <v-row
+            wrap
+            justify="space-between"
+        >
+            <v-col
+                cols="12"
+                class="d-flex d-md-none my-0 py-0"
+            >
+                <v-row wrap justify="space-around" class="my-0 py-0">
+                    <v-col cols="6">
+                        <v-btn
+                            color="primary"
+                            depressed
+                            outlined
+                            tile
+                            block
+                        >Clear Filters</v-btn>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-btn
+                            @click="dialog = !dialog"
+                            color="primary"
+                            depressed
+                            outlined
+                            tile
+                            block
+                        >Apply Filters</v-btn>
+                    </v-col>
+                </v-row>
             </v-col>
-            <v-col cols="12" md="9" lg="9">
-                <AllProducts />
+            <v-col
+                cols="12"
+                md="3"
+                lg="3"
+            >
+                <FilterSection class="d-none d-md-flex" />
+                <MobileFilterSection
+                    class="d-flex d-md-none"
+                    :dialog="dialog"
+                />
+            </v-col>
+            <v-col
+                cols="12"
+                md="9"
+                lg="9"
+            >
+                <AllProducts class="d-none d-md-flex" />
+                <MobileAllProducts class="d-flex d-md-none" />
             </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script>
-import AllProducts from '@/components/Menu/AllProducts';
-import FilterSection from '@/components/Menu/FilterSection';
+import AllProducts from "@/components/Menu/AllProducts";
+import FilterSection from "@/components/Menu/FilterSection";
+
+import MobileAllProducts from "@/components/Menu/Mobile/AllProducts";
+import MobileFilterSection from "@/components/Menu/Mobile/FilterDrawer";
 
 export default {
     components: {
         AllProducts,
-        FilterSection
+        FilterSection,
+        MobileAllProducts,
+        MobileFilterSection
+    },
+    data() {
+        return {
+            dialog: false
+        };
     }
-}
+};
 </script>
