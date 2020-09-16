8bb31a4390933e27a4267f25e3d54bc2fd4e11dc
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-09-16 16:32:49 +0530

    Fixed Bugs

--- src/components/Common/SingleSmallCard.vue
+++ src/components/Common/SingleSmallCard.vue
@@ -5,7 +5,7 @@
 
         <v-row justify="center">
             <v-img
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                :src="item.image ? item.image : `https://cdn.vuetifyjs.com/images/cards/cooking.png`"
                 lazy-src="https://fitmirchi.com/admin/assets/images/image_not_available.png"
                 aspect-ratio="1"
                 class="grey lighten-2"
