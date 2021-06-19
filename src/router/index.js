c3bac7cecdedba48584611dba513133ce93b59c9
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-06-20 01:04:53 +0530

    Updated Bug that pointed local URL Over Server URL

--- src/router/index.js
+++ src/router/index.js
@@ -1,109 +1,114 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 // import NProgress from 'nprogress'
-import store from '@/store'
+import store from "@/store";
 
-import EssentialRoutes from './essential'
-import AccountsRoutes from './accounts'
-import FoodRoutes from './foods'
-import GroceryRoutes from './grocery'
-import OrdersRoutes from './orders'
+import EssentialRoutes from "./essential";
+import AccountsRoutes from "./accounts";
+import FoodRoutes from "./foods";
+import GroceryRoutes from "./grocery";
+import OrdersRoutes from "./orders";
 
-Vue.use(VueRouter)
-var allRoutes = []
+Vue.use(VueRouter);
+var allRoutes = [];
 
 allRoutes = allRoutes.concat(
   ...EssentialRoutes,
   ...AccountsRoutes,
   ...FoodRoutes,
   ...GroceryRoutes,
-  ...OrdersRoutes,
-)
+  ...OrdersRoutes
+);
 
-const routes = allRoutes
+const routes = allRoutes;
 
 const router = new VueRouter({
-  mode: 'hash',
+  mode: "hash",
   base: process.env.BASE_URL,
-  routes
-})
-
+  routes,
+});
 
 router.beforeResolve((to, from, next) => {
   // If this isn't an initial page load.
-  store.dispatch('startLoading')
-    .then(() => {
-      setTimeout(() => {
-
-        next()
-        console.log(to, from)
-      }, 200)
-    })
-})
+  store.dispatch("startLoading").then(() => {
+    setTimeout(() => {
+      next();
+      console.log(to, from);
+    }, 200);
+  });
+});
 
 router.afterEach((to, from) => {
   // Complete the animation of the route progress bar.
   setTimeout(() => {
-    store.dispatch('stopLoading')
-    console.log(to, from)
-  }, 200)
-})
-
-
+    store.dispatch("stopLoading");
+    console.log(to, from);
+  }, 200);
+});
 
 // This callback runs before every route change, including on page load.
 router.beforeEach((to, from, next) => {
   // This goes through the matched routes from last to first, finding the closest route with a title.
   // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
-  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+  const nearestWithTitle = to.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.title);
 
   // Find the nearest route element with meta tags.
-  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
-  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+  const nearestWithMeta = to.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.metaTags);
+  const previousNearestWithMeta = from.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.metaTags);
   console.log(previousNearestWithMeta);
 
   // If a route with a title was found, set the document (page) title to that value.
   if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
 
   // Remove any stale meta tags from the document using the key attribute we set below.
-  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+  Array.from(
+    document.querySelectorAll("[data-vue-router-controlled]")
+  ).map((el) => el.parentNode.removeChild(el));
 
   // Skip rendering meta tags if there are none.
   if (!nearestWithMeta) return next();
 
   // Turn the meta tag definitions into actual elements in the head.
-  nearestWithMeta.meta.metaTags.map(tagDef => {
-      const tag = document.createElement('meta');
+  nearestWithMeta.meta.metaTags
+    .map((tagDef) => {
+      const tag = document.createElement("meta");
 
-      Object.keys(tagDef).forEach(key => {
+      Object.keys(tagDef).forEach((key) => {
         tag.setAttribute(key, tagDef[key]);
       });
 
       // We use this to track which meta tags we create, so we don't interfere with other ones.
-      tag.setAttribute('data-vue-router-controlled', '');
+      tag.setAttribute("data-vue-router-controlled", "");
 
       return tag;
     })
     // Add the meta tags to the document head.
-    .forEach(tag => document.head.appendChild(tag));
+    .forEach((tag) => document.head.appendChild(tag));
 
   next();
 });
 
-
-
 // Handling Unauthenticated Requests
-router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (store.getters.isLoggedIn) {
-      next()
-      return
-    }
-    next('/login')
-  } else {
-    next()
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some(record => record.meta.requiresAuth)) {
+//     if (store.getters.isLoggedIn) {
+//       next()
+//       return
+//     }
+//     next('/login')
+//   } else {
+//     next()
+//   }
+// })
 
 // Handling Dboy Requests
 // router.beforeEach((to, from, next) => {
@@ -118,5 +123,4 @@ router.beforeEach((to, from, next) => {
 //   }
 // })
 
-
-export default router
+export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,88 +1,86 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
+import Vue from "vue";
+import Vuex from "vuex";
 
 // Modules
-import auth from '@/store/modules/auth'
-import cart from '@/store/modules/cart'
-import dboy from '@/store/modules/dboy'
-import foodMenu from '@/store/modules/foodMenu'
-import grocery from '@/store/modules/grocery'
-import wishlist from '@/store/modules/wishlist'
+import auth from "@/store/modules/auth";
+import cart from "@/store/modules/cart";
+import dboy from "@/store/modules/dboy";
+import foodMenu from "@/store/modules/foodMenu";
+import grocery from "@/store/modules/grocery";
+import wishlist from "@/store/modules/wishlist";
 
-Vue.use(Vuex)
-
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    apiUrl: "http://127.0.0.1:8000/api/v1/",
+    apiUrl: "https://easyeats-api-v1.herokuapp.com/api/v1/",
     overlay: false,
     version2: false,
     appVersion: "1.0.0",
-    blocked: localStorage.getItem('blocked') || false,
-    acceptedVersion: localStorage.getItem('acceptedVersion') || false,
-    cartReloaded: localStorage.getItem('cartReloaded') || false,
+    blocked: localStorage.getItem("blocked") || false,
+    acceptedVersion: localStorage.getItem("acceptedVersion") || false,
+    cartReloaded: localStorage.getItem("cartReloaded") || false,
   },
   getters: {
-    getOverlay: state => !!state.overlay,
-    getBlocked: state => !!state.blocked,
-    getVersion: state => !!state.version2,
-    getAppVersion: state => state.appVersion,
-    getAcceptedVersion: state => !!state.acceptedVersion,
-    getCartReloaded: state => !!state.cartReloaded
+    getOverlay: (state) => !!state.overlay,
+    getBlocked: (state) => !!state.blocked,
+    getVersion: (state) => !!state.version2,
+    getAppVersion: (state) => state.appVersion,
+    getAcceptedVersion: (state) => !!state.acceptedVersion,
+    getCartReloaded: (state) => !!state.cartReloaded,
   },
   actions: {
-    startLoading({commit}) {
-      commit('START_LOADING')
+    startLoading({ commit }) {
+      commit("START_LOADING");
     },
-    stopLoading({commit}) {
-      commit('STOP_LOADING')
+    stopLoading({ commit }) {
+      commit("STOP_LOADING");
     },
-    acceptNewUpdate({commit}) {
-      commit('ACCEPT_NEW_UPDATE')
+    acceptNewUpdate({ commit }) {
+      commit("ACCEPT_NEW_UPDATE");
     },
-    setCartReloaded({commit}) {
-      commit('SET_CART_RELOADED')
+    setCartReloaded({ commit }) {
+      commit("SET_CART_RELOADED");
     },
-    setCartUnloaded({commit}) {
-      commit('SET_CART_UNLOADED')
+    setCartUnloaded({ commit }) {
+      commit("SET_CART_UNLOADED");
     },
-    blockSite({commit}) {
-      commit('BLOCK_SITE')
+    blockSite({ commit }) {
+      commit("BLOCK_SITE");
     },
-    unblockSite({commit}) {
-      commit('UNBLOCK_SITE')
-    }
+    unblockSite({ commit }) {
+      commit("UNBLOCK_SITE");
+    },
   },
   mutations: {
     START_LOADING(state) {
-      state.overlay = true
+      state.overlay = true;
     },
     STOP_LOADING(state) {
-      state.overlay = false
+      state.overlay = false;
     },
     ACCEPT_NEW_UPDATE(state) {
-      state.acceptedVersion = true
-      localStorage.setItem('acceptedVersion', true)
+      state.acceptedVersion = true;
+      localStorage.setItem("acceptedVersion", true);
     },
     SET_CART_RELOADED(state) {
-      state.cartReloaded = true
-      localStorage.setItem('cartReloaded', true)
+      state.cartReloaded = true;
+      localStorage.setItem("cartReloaded", true);
     },
 
     SET_CART_UNLOADED(state) {
-      state.cartReloaded = false
-      localStorage.setItem('cartReloaded', false)
+      state.cartReloaded = false;
+      localStorage.setItem("cartReloaded", false);
     },
     BLOCK_SITE(state) {
-      state.blocked = true
-      localStorage.setItem('blocked', true)
+      state.blocked = true;
+      localStorage.setItem("blocked", true);
     },
 
     UNBLOCK_SITE(state) {
-      state.blocked = false
-      localStorage.setItem('blocked', false)
-    }
+      state.blocked = false;
+      localStorage.setItem("blocked", false);
+    },
   },
   modules: {
     auth,
@@ -91,5 +89,5 @@ export default new Vuex.Store({
     foodMenu,
     grocery,
     wishlist,
-  }
-})
+  },
+});
