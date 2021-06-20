418f90b533a389db0197df7954d5a80cf19539f8
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-06-20 09:32:15 +0530

    Fixed the apiUrl bug by changing it to env variable

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,120 +1,42 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 const state = {
-    user: localStorage.getItem('user') || {},
-    status: localStorage.getItem('status') || false,
-    token: localStorage.getItem('token') || "",
-    otpSent : false,
-    otpVerified : false
+  user: localStorage.getItem("user") || {},
+  status: localStorage.getItem("status") || false,
+  token: localStorage.getItem("token") || "",
+  otpSent: false,
+  otpVerified: false,
 };
 
 const getters = {
-    isLoggedIn: state => !!state.status,
-    getUser: state => JSON.parse(state.user),
-    getToken: state => state.token
+  isLoggedIn: (state) => !!state.status,
+  getUser: (state) => JSON.parse(state.user),
+  getToken: (state) => state.token,
 };
 
 const actions = {
-    loginUser({
-        commit
-    }, data) {
+  loginUser({ commit }, data) {
+    return new Promise((resolve, reject) => {
+      console.log(data);
+      axios({
+        url: `${process.env.apiUrl}accounts/login/`,
+        data: data,
+        method: "POST",
+      })
+        .then((response) => {
+          localStorage.setItem("token", response.data.token);
+          localStorage.setItem("status", true);
+          commit("LOGIN_USER", response.data.token);
 
-        return new Promise((resolve, reject) => {
-            console.log(data)
-            axios({
-                    url: `${this.state.apiUrl}accounts/login/`,
-                    data: data,
-                    method: 'POST'
-                })
-                .then(response => {
-                    localStorage.setItem('token', response.data.token)
-                    localStorage.setItem('status', true)
-                    commit('LOGIN_USER', response.data.token);
-
-                    axios({
-                            url: `${this.state.apiUrl}accounts/users/${data.username}/`,
-                            method: 'GET',
-                            headers: {
-                                'Authorization': `Token ${response.data.token}`
-                            }
-                        })
-                        .then(resp => {
-                            let user = {
-                                username: resp.data.user_id,
-                                email: resp.data.email,
-                                full_name: resp.data.full_name,
-                                gender: resp.data.gender,
-                                id: resp.data.id,
-                                is_admin: resp.data.is_admin,
-                                is_client: resp.data.is_client,
-                                is_delivery: resp.data.is_delivery,
-                                is_newsletter: resp.data.is_newsletter,
-                                is_vendor: resp.data.is_vendor,
-                                mobile_number: resp.data.mobile_number,
-                                order_count: resp.data.order_count,
-                                password: resp.data.password
-                            }
-                            localStorage.setItem('user', JSON.stringify(user))
-                            commit('SET_USER', JSON.stringify(user))
-                            axios({
-                                url: `${this.state.apiUrl}cart/create/`,
-                                method: 'POST',
-                                headers: {
-                                    'Authorization': `Token ${response.data.token}`
-                                }
-                            })
-                            .then(res => {
-                                localStorage.setItem('currentCart', res.data.id)
-                                localStorage.setItem('cartItems', res.data.count)
-                            })
-                            .catch(err => console.log(err))
-                        })
-                        .catch(err => {
-                            console.log(err)
-                        })
-                    resolve(response)
-                })
-                .catch(err => {
-                    localStorage.removeItem('token')
-                    localStorage.removeItem('status')
-                    console.log(err)
-                    reject(err)
-                })
-        });
-    },
-
-    registerUser({
-        commit
-    }, data) {
-
-        return new Promise((resolve, reject) => {
-            axios({
-                url: `${this.state.apiUrl}accounts/register/`,
-                data: data,
-                method: 'POST'
-            })
-            .then(response => {
-                console.log(commit)
-                resolve(response)
-            })
-            .catch(err => {
-                console.log(err)
-                reject(err)
-            })
-        })
-    },
-
-    syncProfile({commit}, username) {
-        axios({
-            url: `${this.state.apiUrl}accounts/users/${username}/`,
-            method: 'GET',
+          axios({
+            url: `${process.env.apiUrl}accounts/users/${data.username}/`,
+            method: "GET",
             headers: {
-                'Authorization': `Token ${this.getters.getToken}`
-            }
-        })
-        .then(resp => {
-            let user = {
+              Authorization: `Token ${response.data.token}`,
+            },
+          })
+            .then((resp) => {
+              let user = {
                 username: resp.data.user_id,
                 email: resp.data.email,
                 full_name: resp.data.full_name,
@@ -127,67 +49,132 @@ const actions = {
                 is_vendor: resp.data.is_vendor,
                 mobile_number: resp.data.mobile_number,
                 order_count: resp.data.order_count,
-                password: resp.data.password
-            }
-            localStorage.setItem('user', JSON.stringify(user))
-            commit('SET_USER', JSON.stringify(user))
+                password: resp.data.password,
+              };
+              localStorage.setItem("user", JSON.stringify(user));
+              commit("SET_USER", JSON.stringify(user));
+              axios({
+                url: `${process.env.apiUrl}cart/create/`,
+                method: "POST",
+                headers: {
+                  Authorization: `Token ${response.data.token}`,
+                },
+              })
+                .then((res) => {
+                  localStorage.setItem("currentCart", res.data.id);
+                  localStorage.setItem("cartItems", res.data.count);
+                })
+                .catch((err) => console.log(err));
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+          resolve(response);
         })
-        .catch(err => {
-            console.log(err)
-        })
-    },
+        .catch((err) => {
+          localStorage.removeItem("token");
+          localStorage.removeItem("status");
+          console.log(err);
+          reject(err);
+        });
+    });
+  },
 
-    logoutUser({
-        commit
-    }) {
-        localStorage.clear();
-        commit('LOGOUT_USER');
-    },
-    sendOtp({
-        commit
-    }) {
-        commit('SEND_OTP', true);
-    },
-    verifyOtp({
-        commit
-    }) {
-        commit('VERIFY_OTP', true);
-    }
+  registerUser({ commit }, data) {
+    return new Promise((resolve, reject) => {
+      axios({
+        url: `${process.env.apiUrl}accounts/register/`,
+        data: data,
+        method: "POST",
+      })
+        .then((response) => {
+          console.log(commit);
+          resolve(response);
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  },
+
+  syncProfile({ commit }, username) {
+    axios({
+      url: `${process.env.apiUrl}accounts/users/${username}/`,
+      method: "GET",
+      headers: {
+        Authorization: `Token ${this.getters.getToken}`,
+      },
+    })
+      .then((resp) => {
+        let user = {
+          username: resp.data.user_id,
+          email: resp.data.email,
+          full_name: resp.data.full_name,
+          gender: resp.data.gender,
+          id: resp.data.id,
+          is_admin: resp.data.is_admin,
+          is_client: resp.data.is_client,
+          is_delivery: resp.data.is_delivery,
+          is_newsletter: resp.data.is_newsletter,
+          is_vendor: resp.data.is_vendor,
+          mobile_number: resp.data.mobile_number,
+          order_count: resp.data.order_count,
+          password: resp.data.password,
+        };
+        localStorage.setItem("user", JSON.stringify(user));
+        commit("SET_USER", JSON.stringify(user));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+
+  logoutUser({ commit }) {
+    localStorage.clear();
+    commit("LOGOUT_USER");
+  },
+  sendOtp({ commit }) {
+    commit("SEND_OTP", true);
+  },
+  verifyOtp({ commit }) {
+    commit("VERIFY_OTP", true);
+  },
 };
 
 const mutations = {
-    SEND_OTP(state, response) {
-        state.otpSent = response;
-    },
+  SEND_OTP(state, response) {
+    state.otpSent = response;
+  },
 
-    VERIFY_OTP(state, response) {
-        state.otpVerified = response;
-    },
-    LOGIN_USER(state, response) {
-        state.user = {}
-        state.token = response
-        state.status = true
-    },
+  VERIFY_OTP(state, response) {
+    state.otpVerified = response;
+  },
+  LOGIN_USER(state, response) {
+    state.user = {};
+    state.token = response;
+    state.status = true;
+  },
 
-    SET_USER(state, response) {
-        state.user = response
-    },
+  SET_USER(state, response) {
+    state.user = response;
+  },
 
-    LOGOUT_USER(state) {
-        state.user = {};
-        state.status = false;
-        state.foodCart = [];
-        state.groceryCart = [];
-        state.cartItems = 0;
-        state.cartTotal = 0.00;
-        state.addressForCurrentOrder = {};
-        state.cartFromServer = {};
-    }
+  LOGOUT_USER(state) {
+    state.user = {};
+    state.status = false;
+    state.foodCart = [];
+    state.groceryCart = [];
+    state.cartItems = 0;
+    state.cartTotal = 0.0;
+    state.addressForCurrentOrder = {};
+    state.cartFromServer = {};
+  },
 };
 
 export default {
-    state,
-    getters,
-    actions,
-    mutations
-}
+  state,
+  getters,
+  actions,
+  mutations,
+};
